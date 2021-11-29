d9309c15c4a63f3dfc030dbeeb5a1ae2aa59ef57
jewel <jmbengal.mbstu@gmail.com>
2021-11-29 17:08:56 +0600

    category, subcategory & blog post tables are added

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+class category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class subCategory(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class blogPost(models.Model):
+    category = models.ForeignKey(category, on_delete=models.SET_NULL, null=True, blank=True )
+    subCategory = models.ForeignKey(subCategory, on_delete=models.SET_NULL, null=True, blank=True )
+    date = models.DateField(default=datetime.now)
+    title = models.CharField(max_length=500)
+    desc = models.TextField(null=True,blank=True)
+
