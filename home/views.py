e17a83e28f420586ea1c017ceb8509e6f34bebb6
jewel <jmbengal.mbstu@gmail.com>
2021-12-01 22:48:56 +0600

    rendering for each component done

--- home/views.py
+++ home/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
-from home import views
+from blog import models as BMODEL
 # Create your views here.
 
 def home_view(request):
-    return render(request, 'home/home.html')
+    blogs = BMODEL.blogPost.objects.all()
+    singleBlog = BMODEL.blogPost.objects.all().first()
+    if request.method=='GET':
+        dc=request.GET
+        pk=dc.get('id')
+        print(type(pk))
+        if pk != None :
+            singleBlog = BMODEL.blogPost.objects.get(id=pk)
+        # print(f"type of get: {type(request.GET)}" )
+        # print(f"get methods are : {dc.get('id')}")
+        context= {
+        'blogs':blogs,
+        'singleBlog' : singleBlog,
+    }
+    return render(request, 'home/home.html', context)
