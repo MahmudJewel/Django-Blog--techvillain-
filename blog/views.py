b0d09711ade1cb61b0c9bda69c16225cdb2ecc7c
jewel <jmbengal.mbstu@gmail.com>
2021-12-03 19:48:24 +0600

    Index heading is added

--- blog/views.py
+++ blog/views.py
@@ -6,26 +6,29 @@ from blog import models as BMODEL
 # from blog import forms as BFORM
 # Create your views here.
 
+
 def home_view(request):
     return render(request, 'home/home.html')
 
 
-def category_view(request,pk):
+def category_view(request, pk):
     blogs = BMODEL.blogPost.objects.filter(category=pk)
-    singleBlog = blogs.first() # get the first object
-    # category = BMODEL.category.objects.all()
+    singleBlog = blogs.first()  # get the first object
+    # category heading for left side
+    category = BMODEL.category.objects.get(id=pk)
+    print(f"category: {category}")
     # print(f"testing category from {category}")
-    if request.method=='GET':
-        dc=request.GET
-        pk=dc.get('getid') # getid from url link
+    if request.method == 'GET':
+        dc = request.GET
+        pk = dc.get('getid')  # getid from url link
         # print(type(pk))
-        if pk != None :
+        if pk != None:
             singleBlog = BMODEL.blogPost.objects.get(id=pk)
         # print(f"type of get: {type(request.GET)}" )
         # print(f"get methods are : {dc.get('id')}")
-        context= {
-        'blogs':blogs,
-        'singleBlog' : singleBlog,
-        # 'category' : category,
-    }
+        context = {
+            'blogs': blogs,
+            'singleBlog': singleBlog,
+            'category': category,
+        }
     return render(request, 'home/home.html', context)
