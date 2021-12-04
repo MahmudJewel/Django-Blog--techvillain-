3ff721031b556ed5f53b434bca80e33f26bd63df
jewel <jmbengal.mbstu@gmail.com>
2021-12-04 20:48:06 +0600

    search item added

--- blog/views.py
+++ blog/views.py
@@ -6,6 +6,13 @@ from blog import models as BMODEL
 # from blog import forms as BFORM
 # Create your views here.
 
+def search_view(request):
+    if request.method == 'POST':
+        search = request.POST.get('search')
+        print(f"search : {search}")
+        searchItem = BMODEL.blogPost.objects.filter(title__contains = search)
+    return render(request, 'home/search.html',{'searchItem':searchItem})
+
 
 def home_view(request):
     return render(request, 'home/home.html')
@@ -16,7 +23,7 @@ def category_view(request, pk):
     singleBlog = blogs.first()  # get the first object
     # category heading for left side
     category = BMODEL.category.objects.get(id=pk)
-    print(f"category: {category}")
+    # print(f"category: {category}")
     # print(f"testing category from {category}")
     if request.method == 'GET':
         dc = request.GET
@@ -26,9 +33,10 @@ def category_view(request, pk):
             singleBlog = BMODEL.blogPost.objects.get(id=pk)
         # print(f"type of get: {type(request.GET)}" )
         # print(f"get methods are : {dc.get('id')}")
-        context = {
-            'blogs': blogs,
-            'singleBlog': singleBlog,
-            'category': category,
+
+    context = {
+        'blogs': blogs,
+        'singleBlog': singleBlog,
+        'category': category,
         }
     return render(request, 'home/home.html', context)
