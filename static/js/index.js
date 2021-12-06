d66abab8fd15fe2456d838bb4e309485b22616f2
jewel <jmbengal.mbstu@gmail.com>
2021-12-06 23:09:22 +0600

    frontend design for article page done with responsive ham burger

--- static/js/index.js
+++ static/js/index.js
@@ -12,3 +12,18 @@ console.log('Js connected !!!')
 //     console.log(this.className)
 //   });
 // }
+
+
+// responsive left-index 
+const burger=document.querySelector(".burger");
+const nav=document.querySelector(".nav_right");
+const bb=document.querySelector(".bb");
+
+ // for sliding navbar
+burger.addEventListener('click', navSlide);  
+bb.addEventListener('click', navSlide);
+
+function navSlide(){
+	nav.classList.toggle('nav_active');
+	burger.classList.toggle('toggle');
+}
